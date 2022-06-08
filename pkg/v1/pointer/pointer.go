86083aeabe0ff2585c410c220057a7ea9bd54aac
daniil_stepanenko <daniil_stepanenko@epam.com>
2022-06-08 12:55:14 +0300

    simplify code

--- pkg/v1/pointer/pointer.go
+++ pkg/v1/pointer/pointer.go
@@ -1,14 +1,7 @@
 package pointer
 
-type Pointer[T any] interface {
-	*T
-}
-
 // GetValue returns value by ptr if ptr is not nil, zero value
-// GetValue returns value by ptr if Pointer[T] is non-nil, otherwise zero value of T
-// TODO:
-func GetValue[T any, Ptr Pointer[T]](ptr Ptr) (val T) {
-	// func GetValue[T any](ptr *T) (val T) {
+func GetValue[T any](ptr *T) (val T) {
 	if ptr != nil {
 		val = *ptr
 	}
